cc6613732b384024b0a6d97d9e9eb5210a5bccfb
abuzarhimedti <abuzar2233r@gmail.com>
2026-06-10 13:40:09 +0300

    add login

--- app.py
+++ app.py
@@ -5,30 +5,6 @@ import json
 import os
 from datetime import datetime
 
-credentials = {
-    "usernames": {
-        "user1": {
-            "name": "المستخدم الأول",
-            "password": "123456"
-        }
-    }
-}
-
-authenticator = stauth.Authenticate(
-    credentials,
-    "my_app",
-    "secret_key_123",
-    cookie_expiry_days=30
-)
-
-name, authentication_status, username = authenticator.login("تسجيل الدخول", "main")
-
-if authentication_status == False:
-    st.error("اسم المستخدم أو كلمة المرور خاطئة")
-    st.stop()
-elif authentication_status == None:
-    st.warning("أدخل بياناتك")
-    st.stop()
 
 st.set_page_config(page_title="نظام sudan 249  الطبي الذكي", page_icon="🏥", layout="wide")
 
